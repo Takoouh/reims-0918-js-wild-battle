491365fb29c4c2dc27b8f1d16c373ed0a83f3d86
Fabien Raymond <voortexxx@gmail.com>
2018-11-13 21:39:58 +0100

    Updates

---
+++ precache-manifest.5697ae9cdd18bc62ad828752d8e47ad4.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb559b17c7e3db2d8a2c4746b2d832e3",
+    "url": "/static/media/Clement.eb559b17.png"
+  },
+  {
+    "revision": "f6bbe0cf2dbf72d0015c",
+    "url": "/static/css/main.4e0defc3.chunk.css"
+  },
+  {
+    "revision": "0d2541273656e2e7bae7fb0832126da5",
+    "url": "/static/media/list.0d254127.png"
+  },
+  {
+    "revision": "0b5038a6d85094387991",
+    "url": "/static/js/1.0b5038a6.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f721e8eba82a9476647225c6928058a3",
+    "url": "/static/media/logo.f721e8eb.svg"
+  },
+  {
+    "revision": "f6bbe0cf2dbf72d0015c",
+    "url": "/static/js/main.f6bbe0cf.chunk.js"
+  },
+  {
+    "revision": "7dd3a24e22a54242687fb5a49ae75477",
+    "url": "/static/media/Thomas.7dd3a24e.png"
+  },
+  {
+    "revision": "61c8ea6dc493d4da0a5e330753355215",
+    "url": "/static/media/circleBackground.61c8ea6d.svg"
+  },
+  {
+    "revision": "40f5cf89f1b6563fbdbb510f39f6436f",
+    "url": "/static/media/fight.40f5cf89.png"
+  },
+  {
+    "revision": "050f56ddb5df4ac6436497c102e58fe1",
+    "url": "/static/media/stats.050f56dd.png"
+  },
+  {
+    "revision": "0b5038a6d85094387991",
+    "url": "/static/css/1.add314b6.chunk.css"
+  },
+  {
+    "revision": "faf1e74601b6eb6e224b4cec612ea241",
+    "url": "/index.html"
+  }
+];
